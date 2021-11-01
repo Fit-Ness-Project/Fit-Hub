ce767b4c503c1c28518ed9aefa16fff66cb62654
dhafer1hmidi <hmididhafer@yahoo.com>
2021-11-01 16:48:52 +0100

    get The data of the user age weight height 0.1

--- client/FitHub-Project/screens/TabOneScreen.tsx
+++ client/FitHub-Project/screens/TabOneScreen.tsx
@@ -2,32 +2,88 @@ import * as React from 'react';
 import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import { Text, View,  TouchableOpacity, TextInput } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
-  );
-}
+    <View style = {styles.container}> 
+    <Text style={styles.title}>Add You Information for us to help You</Text>
+    <Text style = {styles.label}>Age </Text>
+    <TextInput style= {styles.input} 
+    underlineColorAndroid = "transparent"
+    placeholder = "Age (year)"
+    autoCapitalize = "none"
+  
+    />
 
+    <Text  style = {styles.label}>Height</Text>
+    <TextInput style = {styles.input}
+       underlineColorAndroid = "transparent"
+       placeholder = "Height (Cm)"
+       autoCapitalize = "none"
+       />
+<Text  style = {styles.label}>Weight</Text>
+    <TextInput style = {styles.input}
+       underlineColorAndroid = "transparent"
+       placeholder = "Weight (Kg)"
+       autoCapitalize = "none"
+     />
+    
+    <TouchableOpacity
+       style = {styles.submitButton}
+          
+       >
+       <Text style = {styles.submitButtonText}> Calculate </Text>
+    </TouchableOpacity>
+    <Text style = {styles.resultText}></Text>
+    < Text style = {styles.resultText}></Text>
+    < Text style = {styles.resultText}></Text>
+    </View>
+)
+  }
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+     paddingTop: 23,
+     
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  input: {
+     margin: 15,
+     height: 40,
+     borderWidth: 1,
+     padding: 10,
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  submitButton: {
+     backgroundColor: '#e7ff19',
+     padding: 10,
+     margin: 15,
+     height: 40,
   },
-});
+  submitButtonText:{
+     textAlign: "center",
+     color: 'white',
+    // fontWeight:"bold",
+     fontSize: 18,
+  },
+  output:{
+     textAlign: "center",
+     fontSize: 30,
+  },
+  title:{
+     paddingTop:30,
+     paddingBottom:10,
+     textAlign: "center",
+     fontSize: 30,
+     fontWeight:"bold",
+  },
+  resultText:{
+     paddingTop:20,
+     paddingBottom:10,
+     textAlign: "center",
+     fontSize: 30,
+     color: 'blue'
+  },
+  label:{
+     marginLeft: 15,
+  }
+})
