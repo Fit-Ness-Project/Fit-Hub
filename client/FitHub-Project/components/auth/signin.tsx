93e4f8ef30454ff37de391b007ac26d7bc21532b
ghofraneHammoudi <ga.frouna123@gmail.com>
2021-11-05 09:50:27 +0100

    verification done

--- client/FitHub-Project/components/auth/signin.tsx
+++ client/FitHub-Project/components/auth/signin.tsx
@@ -16,10 +16,12 @@ export default function Login({ navigation }) {
   useKeepAwake();
   const validationSchema = Yup.object().shape({
     name: Yup.string().required('Name is required').label('Name'),
+    
     email: Yup.string()
       .email('Please enter valid email')
       .required('Email is required')
       .label('Email'),
+
     password: Yup.string()
       .matches(/\w*[a-z]\w*/, 'Password must have a small letter')
       .matches(/\w*[A-Z]\w*/, 'Password must have a capital letter')
