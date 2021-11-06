c505f13909226a6659be8004c2e8ae2861979bdb
= <=>
2021-11-06 15:31:40 +0100

    first view complete

--- client/FitHub-Project/screens/FirstVue/FirstVue.tsx
+++ client/FitHub-Project/screens/FirstVue/FirstVue.tsx
@@ -11,17 +11,21 @@ const FirstVue = ({
     return (
         <View style ={tw`h-full w-full bg-black`}>
 <ImageBackground style={tw`w-full h-full `} source={require("../../assets/images/back.jpg")}> 
-<View style = {{width : "100%" , height :"100%" , backgroundColor :"transparent"}}>
-<View style = {{width : "100%" , height : "60%",backgroundColor :"transparent"}}>
-
+<View style = {{width : "100%",height :"100%",backgroundColor :"transparent",alignItems: "center"}}>
+<View style = {{width : "100%" , height : "65%",backgroundColor :"transparent"}}>
 </View>
-<View  style = {tw` items-center bg-transparent  `} >
+<View style ={{backgroundColor : "transparent",height :"10%"}}>
+    <Text style ={tw`text-white`}> </Text>
+    </View>
+<View style ={tw`bg-transparent   flex-row `}>
+<View  style = {tw` items-center  bg-transparent  `} >
 <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("signUp")} > 
-<Text style = {tw`text-center font-bold  text-black text-2xl pt-3.5`} >REGISTER</Text></TouchableOpacity>
+<Text style = {tw`text-center font-bold  text-black text-xl pt-3.5`} >REGISTER</Text></TouchableOpacity>
 </View>
-<View  style = {tw`mt-2 items-center  bg-transparent`}>
+<View  style = {tw` items-center pl-4 bg-transparent`}>
 <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("login")} > 
-<Text style = {tw`text-center   font-bold text-black text-2xl pt-3.5`} >LOGIN</Text></TouchableOpacity>
+<Text style = {tw`text-center   font-bold text-black text-xl pt-3.5`} >LOGIN</Text></TouchableOpacity>
+</View>
 </View>
 {/* <Button  onPress={() => navigation.navigate("login")} title="logIn" />            */}
 </View>
@@ -36,7 +40,7 @@ export default FirstVue
 const styles = StyleSheet.create({
     button: {
         borderWidth: 1,
-        width: 285,
+        width: 150,
         borderRadius : 5,
         height: 60, 
         opacity: .8,
