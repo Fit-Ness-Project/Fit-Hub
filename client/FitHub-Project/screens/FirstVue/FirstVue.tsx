fddd0401930e2476666d227b40532d6e4f229bc4
ghofraneHammoudi <ga.frouna123@gmail.com>
2021-11-06 16:03:54 +0100

    homepage in progress

--- client/FitHub-Project/screens/FirstVue/FirstVue.tsx
+++ client/FitHub-Project/screens/FirstVue/FirstVue.tsx
@@ -9,35 +9,7 @@ const FirstVue = ({
 }: RootTabScreenProps<"FirstVueProps">) => {
     const navigation = useNavigation()
     return (
-        <ScrollView>
-
-            <View style={tw`mt-5 h-36 bg-red-200 rounded-xl`} >
-                <ImageBackground style={tw`w-full h-full`} source={require("../../assets/images/gymView2.png")}>
-                </ImageBackground>
-
-            </View>
-            <View style={tw`mt-5 h-36 bg-red-200 rounded-xl`} >
-                <ImageBackground style={tw`w-full h-full`} source={require("../../assets/images/coach.png")}>
-                </ImageBackground>
-
-            </View>
-            <View style={tw`mt-5 h-36 bg-red-200 rounded-xl`} >
-                <ImageBackground style={tw`w-full h-full`} source={require("../../assets/images/resto.png")}>
-                </ImageBackground>
-
-            </View>
-            <View style={tw`mt-5 h-36 bg-red-200 rounded-xl`} >
-                <ImageBackground style={tw`w-full h-full`} source={require("../../assets/images/events.png")}>
-                </ImageBackground>
-            </View>
-            <View style={tw`mt-5 h-36 bg-red-200 rounded-xl`} >
-            <ImageBackground style={tw`w-full h-full`} source={require("../../assets/images/blogs.png")}>
-                </ImageBackground>
-
-            </View>
-
-
-        </ScrollView>
+       <View></View>
     )
 }
 
--- client/FitHub-Project/screens/HomePage/getData/HomeVue/HomeVue.tsx
+++ client/FitHub-Project/screens/HomePage/getData/HomeVue/HomeVue.tsx
@@ -11,29 +11,33 @@ export default function ChangeView({
   const navigation = useNavigation()
   return (
     <ScrollView>
-     
-      <View style={tw`mt-5 h-36 bg-red-200 rounded-xl`} >
+
+    <View style={tw`mt-5 h-36 bg-red-200 rounded-xl`} >
         <ImageBackground style={tw`w-full h-full`} source={require("../../../../assets/images/gymView2.png")}>
         </ImageBackground>
 
-      </View>
-      <View style={tw`mt-5 h-36 bg-red-200 rounded-xl`} >
+    </View>
+    <View style={tw`mt-5 h-36 bg-red-200 rounded-xl`} >
         <ImageBackground style={tw`w-full h-full`} source={require("../../../../assets/images/coach.png")}>
         </ImageBackground>
 
-      </View>
-      <View style={tw`mt-5 h-36 bg-red-200 rounded-xl`} >
-        {/* <ImageBackground style={tw`w-full h-full`} source={require("../../../../assets/images/restoo.jpeg")}>
-        </ImageBackground> */}
-
-      </View>
-      <View style={tw`mt-5 h-36 bg-red-200 rounded-xl`} >
-        <ImageBackground style={tw`w-full h-full`} source={require("../../../../assets/images/events.jpg")}>
+    </View>
+    <View style={tw`mt-5 h-36 bg-red-200 rounded-xl`} >
+        <ImageBackground style={tw`w-full h-full`} source={require("../../../../assets/images/resto.png")}>
         </ImageBackground>
-      </View>
-      <View style={tw`mt-5 h-36 bg-red-200 rounded-xl`} ></View>
+
+    </View>
+    <View style={tw`mt-5 h-36 bg-red-200 rounded-xl`} >
+        <ImageBackground style={tw`w-full h-full`} source={require("../../../../assets/images/events.png")}>
+        </ImageBackground>
+    </View>
+    <View style={tw`mt-5 h-36 bg-red-200 rounded-xl`} >
+    <ImageBackground style={tw`w-full h-full`} source={require("../../../../assets/images/blogs.png")}>
+        </ImageBackground>
+
+    </View>
 
 
-    </ScrollView>
+</ScrollView>
   );
 }
