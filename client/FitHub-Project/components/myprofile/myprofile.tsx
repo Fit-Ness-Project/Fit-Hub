a6b1c7bda43aa7481fc9c164be5b65db88451a95
dhafer1hmidi <hmididhafer@yahoo.com>
2021-11-06 12:11:39 +0100

    gender drop down

---
+++ client/FitHub-Project/components/myprofile/myprofile.tsx
@@ -0,0 +1,16 @@
+import * as  React from 'react';
+import { StyleSheet, Text, View ,SafeAreaView } from 'react-native';
+import { Avatar, Title, Caption, TouchableRipple} from 'react-native-paper';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import Share from 'react-native-share';
+const myprofile = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default myprofile
+
+const styles = StyleSheet.create({})
