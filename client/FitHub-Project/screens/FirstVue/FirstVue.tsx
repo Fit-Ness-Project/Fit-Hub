97b7cbf4bbe3ba15931b117af330f36af91b0700
ghofraneHammoudi <ga.frouna123@gmail.com>
2021-11-07 17:22:29 +0100

    homepage done

--- client/FitHub-Project/screens/FirstVue/FirstVue.tsx
+++ client/FitHub-Project/screens/FirstVue/FirstVue.tsx
@@ -1,59 +1,107 @@
-import  { useEffect, useState , useCallback } from 'react'
+import { useEffect, useState, useCallback } from 'react'
 import * as React from 'react'
-import { StyleSheet , Button,ImageBackground,TouchableOpacity } from 'react-native'
-import {Text, View} from '../../components/Themed';
+import { StyleSheet, Button, ImageBackground, TouchableOpacity, ScrollView, Image } from 'react-native'
+import { Text, View } from '../../components/Themed';
 import { RootTabScreenProps } from '../../types';
 import { useNavigation } from '@react-navigation/native';
 import tw from 'tailwind-react-native-classnames';
-const FirstVue = ({
-}: RootTabScreenProps<"FirstVueProps">) => {
-    const navigation = useNavigation()
-    const hints = [["Exercise Daily","Exercise daily for at least an hour. You do not have to kill yourself from running, jogging, etc., but you should have some sort of moderate physical activity in your everyday life. If you're looking to shed a few pounds fast, do a higher-level intensity workout. For example, go on a walk at a brisk pace for an hour. Or, you can jog and set certain intervals to sprint during that hour. Make sure you're not in severe pain during your workout. Just a warning, your muscles will ache after a high intensity workout. It may be irritating, but that means your body is changing for the better. Be sure to stay hydrated, stretch, and eat foods with a decent amount of protein after each workout. The protein will help keep your muscles, not fat, rebuilding."],
-    ["Stay Motivated","An important key to being in shape is to set goals and keep a positive mindset. If you stay positive, you will be able to push yourself to get that fit body you've always wanted."],
-    ["Be Sure to Get Sleep","Even though most of us have eight-hour jobs during the day or night, it is crucial to get enough sleep to recharge the body's batteries. Six to eight hours of sleep will keep the body going throughout the day, but if you happen to feel tired at any point after coming home from work, by all means take a small nap before exercising. You should only nap for about a half hour. This will prevent you from staying up later in the night."]]
-    const [newName, setnewName] :any  = useState(["Stay Motivated","An important key to being in shape is to set goals and keep a positive mindset. If you stay positive, you will be able to push yourself to get that fit body you've always wanted."]);
+import { borderRadius } from '@mui/system';
+const FirstVue = () => {
 
-    const shuffle = useCallback(() => {
-        const index = Math.floor(Math.random() * hints.length);
-        setnewName(hints[index]);
-    }, []);
 
-    useEffect(() => {
-        const intervalID = setInterval(shuffle, 3500);
-        return () => clearInterval(intervalID);
-    }, [shuffle])
     return (
-        <View style ={tw`h-full w-full bg-black`}>
-<ImageBackground style={tw`w-full h-full `} source={require("../../assets/images/back.jpg")}> 
-<View style = {{width : "100%",height :"100%",backgroundColor :"transparent"}}>
-<View style = {{width : "100%" , height : "65%",backgroundColor :"transparent"}}>
-</View>
-<View style = {tw`bg-black opacity-80`}>
-<View style = {tw`bg-transparent text-left`}>
-<Text style = {tw`font-bold text-white bg-transparent text-3xl`}> {newName[0]}  </Text > 
-</View>
+        <ScrollView>
 
-<View  style ={{backgroundColor : "transparent",height :"25%" ,width:"90%", alignItems: "center"}}>
-    <Text style={tw`text-white `} >{newName[1].slice(0,150)}...  </Text>
-    
-    </View>
-    <View style = {tw`items-center bg-transparent`}>
-<View style ={tw`bg-transparent   flex-row `}>
-<View  style = {tw` items-center  bg-transparent  `} >
-<TouchableOpacity style={styles.button} onPress={() => navigation.navigate("signUp")} > 
-<Text style = {tw`text-center font-bold  text-black text-xl pt-3.5`} >REGISTER</Text></TouchableOpacity>
-</View>
-<View  style = {tw` items-center pl-4 bg-transparent`}>
-<TouchableOpacity style={styles.button} onPress={() => navigation.navigate("login")} > 
-<Text style = {tw`text-center   font-bold text-black text-xl pt-3.5`} >LOGIN</Text></TouchableOpacity>
-</View>
-</View>
-</View>
-</View>
-</View>
-</ImageBackground>
-</View>
-      
+            <View style={tw`mt-5 h-36 bg-red-200 rounded-xl`} >
+                <ImageBackground style={tw`w-full h-full`} source={require("../../assets/images/gymView2.png")}>
+                    <View style={tw`bg-transparent h-8 absolute inset-x-0 bottom-0`} >
+
+                        <View style={styles.view}>
+                            <View style={styles.inview}>
+                                <Image style={tw` w-10 h-10  `} source={require("../../assets/Icons/gym.png")} />
+                                <Text style={tw`text-black  pl-2  `} >GYMS</Text>
+                            </View>
+                        </View>
+
+                    </View>
+                </ImageBackground>
+            </View>
+
+            <View style={tw`mt-5 h-36 bg-red-200 rounded-xl`} >
+
+                <ImageBackground style={tw`w-full h-full`} source={require("../../assets/images/coach.png")}>
+                    <View style={tw`bg-transparent h-8 absolute inset-x-0 bottom-0`} >
+
+                        <View style={styles.view}>
+                            <View style={styles.inview}>
+                                <View style={tw`h-7 w-7 bg-transparent`}>
+
+                                    <Image style={tw` w-full h-full  `} source={require("../../assets/Icons/coach.png")} />
+                                </View>
+                                <Text style={tw`text-black  pl-2  `} >COACH</Text>
+                            </View>
+                        </View>
+
+                    </View>
+                </ImageBackground>
+
+            </View>
+            <View style={tw`mt-5 h-36 bg-red-200 rounded-xl`} >
+                <ImageBackground style={tw`w-full h-full`} source={require("../../assets/images/resto.png")}>
+                    <View style={tw`bg-transparent h-8 absolute inset-x-0 bottom-0`} >
+
+                        <View style={styles.view}>
+                            <View style={styles.inview}>
+                                <View style={tw`h-7 w-8 bg-transparent`}>
+                                    <View style={tw`h-7 w-8 bg-transparent`}>
+
+                                        <Image style={tw` w-full h-full `} source={require("../../assets/Icons/resto.png")} />
+                                    </View>
+                                </View>
+                                <Text style={tw`  text-black  pl-2  `} >RESTAURANTS</Text>
+                            </View>
+                        </View>
+
+                    </View>
+                </ImageBackground>
+            </View>
+            <View style={tw`mt-5 h-36 bg-red-200 rounded-xl`} >
+                <ImageBackground style={tw`w-full h-full`} source={require("../../assets/images/events.png")}>
+                    <View style={tw`bg-transparent h-8 absolute inset-x-0 bottom-0`} >
+
+                        <View style={styles.view}>
+                            <View style={styles.inview}>
+                                <View style={tw`h-7 w-8 bg-transparent`}>
+
+                                    <Image style={tw` w-full h-full  `} source={require("../../assets/Icons/event.png")} />
+                                </View>
+                                <Text style={tw`  text-black  pl-2  `} >EVENTS</Text>
+                            </View>
+                        </View>
+
+                    </View>            </ImageBackground>
+            </View>
+            <View style={tw`mt-5 h-36 bg-red-200 rounded-xl`} >
+                <ImageBackground style={tw`w-full h-full`} source={require("../../assets/images/blogs.png")}>
+                    <View style={tw`bg-transparent h-8 absolute inset-x-0 bottom-0`} >
+
+                        <View style={styles.view}>
+                            <View style={styles.inview}>
+                                <View style={tw`h-7 w-7 bg-transparent`}>
+
+                                    <Image style={tw` w-full h-full  `} source={require("../../assets/Icons/blog.png")} />
+                                </View>
+                                <Text style={tw`  text-black  pl-2  `} >BLOGS</Text>
+                            </View>
+                        </View>
+
+                    </View>
+                </ImageBackground>
+
+            </View>
+
+
+        </ScrollView>
     )
 }
 
@@ -63,12 +111,37 @@ const styles = StyleSheet.create({
     button: {
         borderWidth: 1,
         width: 150,
-        borderRadius : 5,
-        height: 60, 
+        borderRadius: 5,
+        height: 60,
         opacity: .8,
-        fontWeight : "bold",
-        backgroundColor : "#e7ff19",
-        borderColor : "gray",
+        fontWeight: "bold",
+        backgroundColor: "#e7ff19",
+        borderColor: "gray",
 
+    },
+    text: {
+        fontSize: 28,
+    },
+    view: {
+        backgroundColor: "#e7ff19",
+        alignItems: "center",
+        width: "100%",
+        height: "100%",
+        padding: 10,
+        opacity: .6
+    },
+    inview: {
+        backgroundColor: "transparent",
+        flexDirection: "row",
+        height: "100%",
+        alignItems: "center",
+    },
+    holder: {
+        marginTop: -5,
+        height: "36%",
+        backgroundColor: "transparent",
+        borderRadius: 5,
+        alignItems: "center"
     }
+
 })
