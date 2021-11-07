9b1bada561ddf32a47b182a4214daff21e34fd6d
dhafer1hmidi <hmididhafer@yahoo.com>
2021-11-07 15:39:09 +0100

    befor pullin

--- client/FitHub-Project/screens/Events/createEvent.tsx
+++ client/FitHub-Project/screens/Events/createEvent.tsx
@@ -1,54 +1,75 @@
-import React from 'react'
-import { Button, TextInput, Text, View } from 'react-native'
-import { Formik } from 'formik'
-import tw from 'tailwind-react-native-classnames'
+import React, { useState } from "react";
+import { Button, TextInput, Text, View } from "react-native";
+import { Formik } from "formik";
+import tw from "tailwind-react-native-classnames";
+import axios from "axios";
 
-export const createEvent = () => (
+export const createEvent = () => {
+  useState(() => {
+    axios
+      .post("http://192.168.11.134:5000/events", {
+        eventName: "",
+      })
+
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((Error) => {
+        console.log(Error);
+      });
+  });
+
+  return (
     <Formik
-    initialValues={{ eventName: '',adress: '',description: '' , date: '', created_by: ''}}
-    onSubmit={values => console.log(values)}
-  >
-    {({ handleChange, handleBlur, handleSubmit, values }) => (
-      <View style={tw` mt-10 w-4/5 ml-8 flex `}>
-        <TextInput
-         style={tw`mt-4 rounded h-10 bg-white p-2 `}
-         placeholder="eventName"
-          onChangeText={handleChange('eventName')}
-          onBlur={handleBlur('eventName')}
-          value={values.eventName}
-        />
-       <TextInput
-       style={tw`mt-4 rounded h-10 bg-white p-2 `}
-          onChangeText={handleChange('adress')}
-          onBlur={handleBlur('adress')}
-          placeholder="adress"
-          value={values.adress}
-        />
-        <TextInput
-        style={tw`mt-4 rounded h-10 bg-white p-2 `}
-          onChangeText={handleChange('description')}
-          onBlur={handleBlur('description')}
-          placeholder="description"
-          value={values.description}
-        />
-         <TextInput
-         style={tw`mt-4 rounded h-10 bg-white p-2 `}
-          onChangeText={handleChange('date')}
-          placeholder="date"
-          onBlur={handleBlur('date')}
-          value={values.date}
-        />
-         <TextInput
-         style={tw`mt-4 rounded h-10 bg-white p-2 `}
-          onChangeText={handleChange('created_by')}
-          onBlur={handleBlur('created_by')}
-          placeholder="created by"
-          value={values.created_by}
-        />
-        <Button
-        onPress={()=>handleSubmit} title="Submit" />
-      </View>
-    )}
-  </Formik>
-)
-
+      initialValues={{
+        eventName: "",
+        adress: "",
+        description: "",
+        date: "",
+        created_by: "",
+      }}
+      onSubmit={(values) => console.log(values)}
+    >
+      {({ handleChange, handleBlur, handleSubmit, values }) => (
+        <View style={tw` mt-10 w-4/5 ml-8 flex `}>
+          <TextInput
+            style={tw`mt-4 rounded h-10 bg-white p-2 `}
+            placeholder="eventName"
+            onChangeText={handleChange("eventName")}
+            onBlur={handleBlur("eventName")}
+            value={values.eventName}
+          />
+          <TextInput
+            style={tw`mt-4 rounded h-10 bg-white p-2 `}
+            onChangeText={handleChange("adress")}
+            onBlur={handleBlur("adress")}
+            placeholder="adress"
+            value={values.adress}
+          />
+          <TextInput
+            style={tw`mt-4 rounded h-10 bg-white p-2 `}
+            onChangeText={handleChange("description")}
+            onBlur={handleBlur("description")}
+            placeholder="description"
+            value={values.description}
+          />
+          <TextInput
+            style={tw`mt-4 rounded h-10 bg-white p-2 `}
+            onChangeText={handleChange("date")}
+            placeholder="date"
+            onBlur={handleBlur("date")}
+            value={values.date}
+          />
+          <TextInput
+            style={tw`mt-4 rounded h-10 bg-white p-2 `}
+            onChangeText={handleChange("created_by")}
+            onBlur={handleBlur("created_by")}
+            placeholder="created by"
+            value={values.created_by}
+          />
+          <Button onPress={() => handleSubmit} title="Submit" />
+        </View>
+      )}
+    </Formik>
+  );
+};
