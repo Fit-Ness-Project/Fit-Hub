3f8a2712a47902f384cec43472c6ed58937dec3e
dhafer1hmidi <hmididhafer@yahoo.com>
2021-11-07 16:37:18 +0100

    navigation

--- client/FitHub-Project/components/auth/signin.tsx
+++ client/FitHub-Project/components/auth/signin.tsx
@@ -153,7 +153,7 @@ export default function Login({ }: RootTabScreenProps<'Home'>) {
                         </View>
                         <View style={tw`h-10 mt-6 items-center`}>
                             <Text style={tw`text-white  pl-6 pt-4`}>
-                                Don't have an account ? <Text style={tw`text-blue-400 underline`}>Register</Text>
+                                Don't have an account ? <Text  onPress={() => navigation.navigate("register")}  style={tw`text-blue-400 underline`}>Register</Text>
                             </Text>
                         </View>
                     </View>
