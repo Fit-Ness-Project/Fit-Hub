ed405b538539b850615ade0be0aac8cc913c24ba
MohamedAmineYousfi <mohamedamineyousfi@gmail.com>
2021-11-08 13:16:11 +0100

    start restaurants

---
+++ client/FitHub-Project/screens/restaurants/interface.ts
@@ -0,0 +1,11 @@
+export  interface FoodR { 
+        id: number;
+        recipeTitle: string ;
+        created_at: Date;
+        content: string ;
+        user_id: number ;
+        likes: number;
+        comments: string ;
+        imageUrl :string ; 
+    
+}
--- client/FitHub-Project/screens/restaurants/restaurant.tsx
+++ client/FitHub-Project/screens/restaurants/restaurant.tsx
@@ -1,14 +1,145 @@
-import * as React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import * as React from "react";
+import  { useEffect, useState , useRef } from 'react'
+import {RestaurantR} from "./interface";
+import axios from "axios";
+import { StatusBar,
+  StyleSheet,
+  FlatList,
+  Image,
+  Dimensions,
+  Animated,
+  TouchableOpacity,
+  Platform,
+  TextInput
+} from "react-native";
+import {
+  Text,
+  View
+} from "../../components/Themed";
+import { useNavigation } from '@react-navigation/core';
 
-const Restaurant = () => {
-    return (
-        <View>
-            <Text></Text>
+const screenWidth = Dimensions.get('screen').width;
+
+export default function Restaurant() {
+  let flatListRef = useRef<FlatList<RestaurantR> | null >();
+
+  const [foodData, setFoodData] = useState<RestaurantR[]>([]);
+  const [searchData, setSearchData] = useState<RestaurantR[]>([]);
+  const  navigation  = useNavigation()
+
+  useEffect(() => {
+    axios 
+    .get('http://192.168.11.64:5000/recipes') 
+     
+    .then((response)=> {
+       console.log(response.data)
+        setFoodData(response.data)
+        setSearchData(response.data)
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+}, [])
+
+  const renderItems: React.FC<{item:RestaurantR}> = ({item})=> {
+  return <TouchableOpacity  onPress={()=> navigation.navigate('Recipe',{
+         recipeId: item.id,
+         recipeTitle: item.recipeTitle,
+         imageUrl: item.imageUrl,
+         likes: item.likes,
+         content: item.content,
+         created_at: item.created_at 
+  }) }
+  activeOpacity={1} >
+    <View style={styles.container}>
+     <View style={{ display: 'flex',flexDirection: 'row', flex: 8, padding: 10, justifyContent: 'center',   borderRadius: 20,}}>
+     <Image source={{ uri:item.imageUrl}} style={{ width :100,flexDirection:'row',height: 100, borderRadius: 20, backgroundColor: '#EAEAEA'}} /> 
+     
+     <View style={{display: 'flex', flex: 10, padding: 10, justifyContent:'space-around', alignItems: 'center'
+      }} >
+         <Text  style={styles.titleT}  >{item.recipeTitle} </Text>
+         <Text >{item.created_at}  </Text>
+         <Text >{item.likes} Likes </Text>
+     </View>
+     </View>
+     </View>
+  </TouchableOpacity>
+ }
+ 
+return (
+  <View  style={styles.container}>
+     <StatusBar   />
+      <FlatList data={foodData}
+       renderItem={renderItems} 
+       keyExtractor={(item,i)=> i.toString()}
+       showsHorizontalScrollIndicator={false}
+   
+/>
+        <View style= {styles.dotview}>
+            {foodData.map(({},index:number)=> (
+                <TouchableOpacity key={index.toString()}  >
+                </TouchableOpacity>
+            ))}
         </View>
-    )
+     </View>
+)
 }
 
-export default Restaurant
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {     
+    display: 'flex',
+    flex: 1, 
+    width: Dimensions.get('screen').width - 30,
+    margin: 10,
+    borderRadius: 20,
+    backgroundColor: "black",
+    height: 100,
+    justifyContent: 'flex-start',
+    borderWidth: 1,
+    borderColor: '#E5E5E5',
+    flexDirection: 'row',
+},
+    image:{
+      width: 'full' ,
+      height: 230 ,
+      alignItems : 'center',
+      resizeMode: 'cover' ,
+      marginVertical : 10 ,
+    },
+    footer:{
+      flexDirection : 'row',
+      justifyContent: 'space-between',
+      height: 100 ,
+      paddingHorizontal: 50,
+      alignItems : 'center',
+      backgroundColor: '#000' ,
+    },
+    titleT:{
+     color: 'black' ,
+     fontSize: 18 ,
+     fontWeight: 'bold',
+    },
+    carousel:{
+        maxHeight: 300
+    },
+    dotview:{
+      flexDirection : 'row',
+      justifyContent: 'center',
+      marginVertical : 20 ,
+    },
+    circle:{
+        width:10,
+        height:10,
+        backgroundColor: "grey",
+        borderRadius: 50,
+    },
+    title:{
+      color: 'white',
+      fontSize: 25 ,
+      fontWeight: 'bold', 
+      backgroundColor: "black"
+    }
+
+})
