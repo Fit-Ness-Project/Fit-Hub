7ec92eea6133132d2b1d774bf5206719fb391829
= <=>
2021-11-09 19:43:07 +0100

    style gym && gymdescription done

--- client/FitHub-Project/components/Gyms/Gyms.tsx
+++ client/FitHub-Project/components/Gyms/Gyms.tsx
@@ -6,12 +6,12 @@ import {
   Image,
   TouchableOpacity,
   ScrollView,
+  ImageBackground
 } from "react-native";
 import tw from "tailwind-react-native-classnames";
 import { Gym } from "./Gyminterface";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Card } from "react-native-paper";
 import { useNavigation } from "@react-navigation/core";
 export default function Gyms() {
   const [GymData, setGymData] = useState<Gym[]>([]);
@@ -28,69 +28,37 @@ export default function Gyms() {
       });
   }, []);
   return (
-    <ScrollView style={{ backgroundColor: "black" }}>
-      {GymData.map((e, k) => (
-        <Card
-          style={{
-            width: "95%",
-            backgroundColor: "rgba(236, 236, 236, 1)",
-            marginTop: 30,
-            marginLeft: 9,
-          }}
-          key={k}
-        >
-          <Card.Cover source={{ uri: e.imgUrl }} />
-
-          <Card.Content>
-            <View
-              style={{
-                flexDirection: "row",
-                backgroundColor: "transparent",
-                alignItems: "center",
-              }}
-            >
-              <Image
-                style={tw`h-6 w-6  mt-1`}
-                source={require("../../assets/Icons/test.png")}
-              />
-              <Text style={tw`text-black text-lg font-bold`}> {e.gymName}</Text>
-            </View>
-          </Card.Content>
-          <View style={{ width: "100%" }}>
-            <Text style={tw`text-black text-sm m-2 mt-3`}>{e.description}</Text>
-          </View>
-          <View style={{ height: 30 }}></View>
-          <View style={{ alignItems: "center" }}>
-            <TouchableOpacity
-              onPress={() =>
-                navigation.navigate("Gymdescription", {
-                  gymName: e.gymName,
-                  imgUrl: e.imgUrl,
-                  rating: e.rating,
-                  description: e.description,
-                  fields: e.fields,
-                  price: e.price,
-                  latitude: e.latitude,
-                  longitude: e.longitude,
-                  phoneNumber: e.phoneNumber,
-                  adress: e.adress,
-                })
-              }
-              style={{
-                backgroundColor: "#e7ff19",
-                height: 35,
-                width: "100%",
-                alignItems: "center",
-                opacity: 0.7,
-              }}
-            >
-              <Image
-                style={tw`h-8 w-8`}
-                source={require("../../assets/Icons/expand-button.png")}
-              />
-            </TouchableOpacity>
-          </View>
-        </Card>
+    <ScrollView style={{ backgroundColor: "white" }}>
+      {GymData.map((e, k) => (       
+          <View key={k} style={tw`h-52 w-full p-2 bg-transparent  `} >
+                    <TouchableOpacity onPress={() =>
+            navigation.navigate("Gymdescription", {
+              gymName: e.gymName,
+              imgUrl: e.imgUrl,
+              rating: e.rating,
+              description: e.description,
+              fields: e.fields,
+              price: e.price,
+              latitude: e.latitude,
+              longitude: e.longitude,
+              phoneNumber: e.phoneNumber,
+              adress: e.adress,
+            })
+          } >
+                        <ImageBackground style={{ width: "100%", height: "100%" }} source={{uri : e.imgUrl}}>
+                            <View style={tw`bg-transparent h-16 absolute inset-x-0 bottom-16  w-full`}>
+                                <View style={styles.view}>
+                                    <View style={styles.inview}>
+                                        <Image style={tw` w-10 h-10  `} source={require("../../assets/Icons/Gym.png")} />
+                                        <View style={tw`h-10 items-center bg-transparent pt-1 `}>
+                                            <Text style={tw`text-black text-3xl pl-2 font-bold`}>{e.gymName}</Text>
+                                        </View>
+                                    </View>
+                                </View>
+                            </View>
+                        </ImageBackground>
+                    </TouchableOpacity>
+                </View>
       ))}
     </ScrollView>
   );
@@ -110,4 +78,18 @@ const styles = StyleSheet.create({
     height: 1,
     width: "80%",
   },
+  view: {
+    backgroundColor: "#36e08b",
+    alignItems: "center",
+    width: "100%",
+    height: "100%",
+    padding: 10,
+    opacity: .7
+},
+inview: {
+    backgroundColor: "transparent",
+    flexDirection: "row",
+    height: "100%",
+    alignItems: "center",
+}
 });
