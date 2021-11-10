7e6b0ba3f76ace293c174cd5533f93ffa7a88631
“ghofraneHammoudi” <ghofraneHammoudy123@gmail.com>
2021-11-10 23:06:08 +0100

    blog screen almost done2

--- client/FitHub-Project/screens/HomePage/getData/HomeVue/HomeVue.tsx
+++ client/FitHub-Project/screens/HomePage/getData/HomeVue/HomeVue.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
-import { useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 
-import { View, Text, TouchableOpacity,} from "../../../../components/Themed";
+import { View, Text, TouchableOpacity, } from "../../../../components/Themed";
 import { Image, ScrollView, ImageBackground, StyleSheet } from "react-native";
 import tw from "tailwind-react-native-classnames";
 import { useNavigation } from '@react-navigation/native';
@@ -10,61 +10,10 @@ import axios from 'axios'
 
 export default function ChangeView({ }) {
     const navigation = useNavigation()
-    const [like, setLike]: any = useState(false)
-    const likebut = [ <TouchableOpacity  onPress={() => { if (like === false) { setLike(true) } else { setLike(false) } }} > 
-        <Image style={tw` w-8 h-8 ml-80  `} source={require("../../../../assets/Icons/heart.png")} />
-        </TouchableOpacity>,
-     <TouchableOpacity onPress={() => { if (like === false) { setLike(true) } else { setLike(false) } }} >
-     <Image style={tw` w-8 h-8 ml-80  `} source={require("../../../../assets/Icons/love.png")} />
-     </TouchableOpacity>
-    ]
-    const [data,setData]:any=useState([])
-    useEffect(() => {
-        axios
-          .get("http://192.168.11.161:5000/blogs")
-    
-          .then((response) => {
-            setData(response.data);
-          })
-          .catch((Error) => {
-            console.log(Error);
-          });
-      }, []);
+   
     return (
-        <ScrollView>
-            <View style={{ padding: 10, backgroundColor: "" }}>
-                <View style={{ backgroundColor: "white", height: "100%" }}>
-                    <View style={{ backgroundColor: "red", height: 280 }}>
-<ImageBackground  source={require("../../../../assets/images/blog4.png")}> </ImageBackground> 
-                    </View>
-                    <View>
-                        <Text style={{ padding: 15, fontSize: 20, textAlign: "center", fontWeight: "bold" }}>
-                            TITLE OF THE BLOG IS HERE
-                        </Text>
-                        <View>
-                 {!like && (likebut[1])}
-                 {like && (likebut[0])}
-                           
-
-
-                        </View>
-                        <Text style={{ paddingLeft: 20, fontSize: 17 }}>
-                            Author Name
-                        </Text>
-                        <Text style={{ fontSize: 10, color: "gray", marginLeft: 20 }}> created at 99/99/9999</Text>
-                        <Text style={{ padding: 20 }}>
-                            Living a healthier life might seem like a tall order — the nutrition, the exercise, the inner happiness! But having some friendly advice at your disposal, whenever and wherever you need it, makes it easier and more fun. With just a click, these awesome blogs filled with tips, tricks, and personal stories will inspire you on your journey to wellness.
-                            Living a healthier life might seem like a tall order — the nutrition, the exercise, the inner happiness! But having some friendly advice at your disposal, whenever and wherever you need it, makes it easier and more fun. With just a click, these awesome blogs filled with tips, tricks, and personal stories will inspire you on your journey to wellness.
-                            Living a healthier life might seem like a tall order — the nutrition, the exercise, the inner happiness! But having some friendly advice at your disposal, whenever and wherever you need it, makes it easier and more fun. With just a click, these awesome blogs filled with tips, tricks, and personal stories will inspire you on your journey to wellness.
-
-
-                        </Text>
-
-
-                    </View>
-                </View>
-            </View>
-            {/* <View>
+      <ScrollView>
+            <View>
                 <View style={tw`h-52 p-2 bg-white pl-4 pr-4 `}>
                     <TouchableOpacity onPress={() => navigation.navigate("Gym")}>
                         <ImageBackground style={{ width: "100%", height: "100%" }} source={require("../../../../assets/images/gym.png")}>
@@ -169,7 +118,7 @@ export default function ChangeView({ }) {
 
                 </View>
 
-            </View> */}
+            </View> 
 
         </ScrollView>
 
